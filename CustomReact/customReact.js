fbd6b3f71fde726ab0839cac71d350cd43fff9f7
OnlyClones..... <jhaa33716@gmail.com>
2025-12-18 02:35:26 +0530

    Created our Own React Library

---
+++ CustomReact/customReact.js
@@ -0,0 +1,29 @@
+// import { Children } from "react";
+function customRender(reactElement, maincontainer){
+    /* 
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    domElement.setAttribute('href', reactElement.props.href)
+    domElement.setAttribute('target',reactElement.props.target)
+    maincontainer.appendChild(domElement)
+    The above one create problem when there exit more number of Attribute
+    */
+   const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    for(const prop in reactElement.props){
+        if(prop === 'children') continue;
+        domElement.setAttribute(prop, reactElement.props[prop]);
+    }
+    maincontainer.appendChild(domElement)
+}
+
+const reactElement = {
+    type: 'a',
+    props:{
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: "Click me to visit google"
+}
+const maincontainer = document.querySelector('#root')
+customRender(reactElement,maincontainer)
